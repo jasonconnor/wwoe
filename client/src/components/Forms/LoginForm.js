800c23cc467a2166aae219c70d9571f6a72a7ed9
Jason <jasonpconn@gmail.com>
2021-02-06 12:02:53 -0500

    Setup Basic Components and React Router Dom Switch

---
+++ client/src/components/Forms/LoginForm.js
@@ -0,0 +1,11 @@
+import * as S from './style'
+
+const LoginForm = () => {
+  return (
+    <S.Form>
+      LoginForm
+    </S.Form>
+  )
+}
+
+export default LoginForm
---
+++ client/src/components/Forms/RegistrationForm.js
@@ -0,0 +1,11 @@
+import * as S from './style'
+
+const RegistrationForm = () => {
+  return (
+    <S.Form>
+      RegistrationForm
+    </S.Form>
+  )
+}
+
+export default RegistrationForm
